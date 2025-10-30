146a689e016947a21e0ce43f5c5783cf06a05835
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-10-30 11:40:45 +0100

    :feat: implement tests for max and min

--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -11,4 +11,26 @@ describe("Statistics", () => {
       expect(statistics.average([10, 20, 30, 40])).toBe(25);
     });
   });
+
+  describe("min", () => {
+    it("should return 0 for an empty array", () => {
+      expect(statistics.min([])).toBe(0);
+    });
+
+    it("should return the minimum of an array of numbers", () => {
+      expect(statistics.min([2, 4, 6])).toBe(2);
+      expect(statistics.min([10, 20, 30, 40])).toBe(10);
+    });
+  });
+
+  describe("max", () => {
+    it("should return 0 for an empty array", () => {
+      expect(statistics.max([])).toBe(0);
+    });
+
+    it("should return the maximum of an array of numbers", () => {
+      expect(statistics.max([2, 4, 6])).toBe(6);
+      expect(statistics.max([10, 20, 30, 40])).toBe(40);
+    });
+  });
 });
