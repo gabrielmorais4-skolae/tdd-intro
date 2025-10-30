b6d2b1adaef8c43c3581d150906673c4fdd77e16
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-10-30 11:41:49 +0100

    :feat: implement min and max functions

--- src/statistics.js
+++ src/statistics.js
@@ -6,6 +6,20 @@ const statistics = {
     const sum = arrayOfNumbers.reduce((acc, num) => acc + num, 0);
     return sum / arrayOfNumbers.length;
   },
+
+  min(arrayOfNumbers) {
+    if (arrayOfNumbers.length === 0) {
+      return 0;
+    }
+    return Math.min(...arrayOfNumbers);
+  },
+
+  max(arrayOfNumbers) {
+    if (arrayOfNumbers.length === 0) {
+      return 0;
+    }
+    return Math.max(...arrayOfNumbers);
+  },
 };
 
 module.exports = statistics;
