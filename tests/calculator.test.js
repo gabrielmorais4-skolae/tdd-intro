f9cbb3b4844afcd70408cad64d04f2dd66887a12
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-10-30 11:06:35 +0100

    :feat: implement test for add negative numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,10 +1,17 @@
 const calculator = require('../calculator');
 
 describe('Calculator', () => {
-    describe('addition', () => {
+    describe('add positive numbers', () => {
         test('should return the sum of two positive numbers', () => {
             expect(calculator.add(2, 3)).toBe(5);
             expect(calculator.add(10, 15)).toBe(25);
+        });
+    });
+
+    describe('add negative numbers', () => {
+        test('should return the sum of two negative numbers', () => {
+            expect(calculator.add(-2, -3)).toBe(-5);
+            expect(calculator.add(-10, -15)).toBe(-25);
         });
     });
 })
